5e9dc518aaa8ebb1385c7d0577ecba43d56ea2e5
Dani Matuko <matuko305@gmail.com>
2022-09-09 12:36:45 +0300

    add user details route and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import UserDetails from './pages/UserDetails';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <main className='container mx-auto my-5'>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/:id' element={<UserDetails />} />
         </Routes>
       </main>
     </div>
